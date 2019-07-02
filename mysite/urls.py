fe3522978ca9993a4ab3f6a4468fd648af6c9872
rakesh1993it <rakesh190493it@gmail.com>
2019-07-02 09:14:02 +0530

    setup the project

---
+++ mysite/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib import admin
+from django.views.generic import RedirectView
+
+from mysite.core import views
+
+
+urlpatterns = [
+	path('admin/',admin.site.urls),
+    path('', RedirectView.as_view(url='/form/1/'), name='index'),
+    path('form/1/', views.AddressFormView.as_view(template_name='form_1.html'), name='form_1'),
+    path('form/2/', views.AddressFormView.as_view(template_name='form_2.html'), name='form_2'),
+    path('form/3/', views.AddressFormView.as_view(template_name='form_3.html'), name='form_3'),
+    path('form/4/', views.CrispyAddressFormView.as_view(), name='form_4'),
+    path('form/5/', views.CustomFieldFormView.as_view(), name='form_5'),
+    path('success/', views.SuccessView.as_view(), name='success'),
+]
